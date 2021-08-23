17e781fd03925d8d2db45884741318a924513242
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-23 16:59:17 -0300

    fix: exercicio5 :toolbox:

--- exercicio5.py
+++ exercicio5.py
@@ -1,10 +1,2 @@
 inp = input("Digite alguma coisa: ")
-print("")
-if(inp.isalpha()):
-    print(inp + " é uma String!" '\n')
-
-if(inp.isnumeric()):
-    print(inp, " é uma int!" '\n')
-
-if(inp.isalnum() and not(inp.isalpha()) and not(inp.isnumeric())):
-    print(inp, " tem int e String!")
+print(type(inp))
