c78b4affe63e395849e6dcb8f317e6f23aca9e28
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-23 17:23:16 -0300

    feat: exercicio1.3 :rocket:

---
+++ exercicio1.3.py
@@ -0,0 +1,8 @@
+#ler 3 valor e somar eles
+n = []
+
+n.append(int(input("Digite um numero: ")))
+n.append(int(input("Digite outro numero: ")))
+n.append(int(input("Digite outro numero: ")))
+
+print("A soma dos tres numeros é: ",(n[0]+n[1]+n[2]))
