f36802ad3c5d767f278db8aa6d06cdb8f5e92356
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-23 17:00:12 -0300

    feat: exercicio7 :rocket:

---
+++ exercicio7.py
@@ -0,0 +1,7 @@
+#7)	Faça um script que leia duas notas de um aluno e mostre como resultado sua média.
+nota = []
+
+nota.append(int(input("Digite a primeira nota: ")))
+nota.append(int(input("Digite a segunda nota: ")))
+
+print("A media das duas notas é: ", (nota[0]+nota[1])/2)
