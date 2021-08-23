1dc77aa91cadf4f4ab7d52be8492bb29b3d48301
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-23 17:00:33 -0300

    feat: exercicio9 :rocket:

---
+++ exercicio9.py
@@ -0,0 +1,5 @@
+#9)	Faça um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+metros = float(input("Digite um valor em metros: "))
+
+print("O valor em metros {}, convertido em centímetros é {}, e convertido em milímetros é {}".format(metros,metros*100,metros*1000))
