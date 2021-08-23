827d8b934ec5d385f37ac3853455bf5836de0354
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-23 15:05:19 -0300

    Fix: exercicio5 :anger:

--- exercicio5.py
+++ exercicio5.py
@@ -1,3 +1,10 @@
-inp = input("Digite algo: ")
+inp = input("Digite alguma coisa: ")
+print("")
+if(inp.isalpha()):
+    print(inp + " é uma String!" '\n')
 
-print(type(inp))
+if(inp.isnumeric()):
+    print(inp, " é uma int!" '\n')
+
+if(inp.isalnum() and not(inp.isalpha()) and not(inp.isnumeric())):
+    print(inp, " tem int e String!")
