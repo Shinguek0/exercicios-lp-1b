1ea7bcdeab380f545b94396c777a12bc38314d84
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-10-25 17:18:10 -0300

    Feat: exercicio15.L.3 :rocket:

--- exercicioLista3.15.py
+++ exercicioLista3.15.py
@@ -2,22 +2,34 @@
 #perder. Mostre ao final, o total de vitórias consecutivas que o jogador conquistou no jogo.
 
 import random
-jogador = True
+vitorias = 0
+maxvitorias = 0
+cond = ""
 
-while(jogador):
-    choice = int(input("Você escolhe par <1> ou impar <2>: "))
-    num1 = int(input("Escolha um numero de 1 a 5: "))
+while (cond!="N"):
+    choice = 0
+    while(choice<1 or choice>2):
+        choice = int(input("Você escolhe par <1> ou impar <2>: "))
+    
+    num1 = int(input("Escolha um numero de 1 a 10: "))
 
-    num = random.randrange(1,6)
+    num = random.randint(1,10)
     numTotal = num + num1
 
     if((choice==1)and(numTotal%2==0)):
+        vitorias+=1
         print("Você: {}\nComputador: {}\n".format(num1,num))
-        print("Você ganhou!!\n----------------------------------------")
+        print("Você ganhou!!\nVitorias consecutivas:",vitorias,"\n----------------------------------------")
     elif((choice==2)and(numTotal%2!=0)):
+        vitorias+=1
         print("Você: {}\nComputador: {}\n".format(num1,num))
-        print("Você ganhou!!\n----------------------------------------")
+        print("Você ganhou!!\nVitorias consecutivas:",vitorias,"\n----------------------------------------")
     else:
+        vitorias = 0
         print("Você: {}\nComputador: {}\n".format(num1,num))
         print("Você perdeu!\n----------------------------------------")
-        jogador = False
+    if(vitorias>maxvitorias):
+        maxvitorias=vitorias
+    cond = input("Você deseja continuar?\n(S/N) ").upper()
+
+print("Maior numero de vitorias consecutivas: ",maxvitorias)
