c23d46c226b3b86a2124ffb092d533badb1784fb
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-10-25 13:41:41 -0300

    Feat: esxercicio15.L.3 :rocket:

--- exercicioLista3.15.py
+++ exercicioLista3.15.py
@@ -2,14 +2,22 @@
 #perder. Mostre ao final, o total de vitórias consecutivas que o jogador conquistou no jogo.
 
 import random
+jogador = True
 
-choice = int(input("Você escolhe par <1> ou impar <2>"))
+while(jogador):
+    choice = int(input("Você escolhe par <1> ou impar <2>: "))
+    num1 = int(input("Escolha um numero de 1 a 5: "))
 
+    num = random.randrange(1,6)
+    numTotal = num + num1
 
-n = random.randrange(1,2)
-print(n)
-
-if(num%2==0):
-    print("par ganhou")
-else:
-    print("impar ganhou")
+    if((choice==1)and(numTotal%2==0)):
+        print("Você: {}\nComputador: {}\n".format(num1,num))
+        print("Você ganhou!!\n----------------------------------------")
+    elif((choice==2)and(numTotal%2!=0)):
+        print("Você: {}\nComputador: {}\n".format(num1,num))
+        print("Você ganhou!!\n----------------------------------------")
+    else:
+        print("Você: {}\nComputador: {}\n".format(num1,num))
+        print("Você perdeu!\n----------------------------------------")
+        jogador = False
